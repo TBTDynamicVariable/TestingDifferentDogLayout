24d2785a8cbe240e76cf04f24b458f16881ce4ba
Myles Lewis <mglewis@bu.edu>
2019-03-01 11:51:49 -0500

    Updated gradle and manifest, still doesnt understand fragment

--- audioSpectrumAnalyzer/src/main/java/github/bewantbe/audio_analyzer_for_android/toDogActivity.java
+++ audioSpectrumAnalyzer/src/main/java/github/bewantbe/audio_analyzer_for_android/toDogActivity.java
@@ -4,19 +4,24 @@ import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
-import android.widget.Button;
+
+import
+import com.wowwee.bluetoothrobotcontrollib.chip.ChipRobot;
+import com.wowwee.bluetoothrobotcontrollib.chip.ChipRobotFinder;
+import github.bewantbe.audio_analyzer_for_android.ConnectFragment;
+import github.bewantbe.audio_analyzer_for_android.utils.FragmentHelper;
 
 import java.util.List;
 
 public class toDogActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todog);
 
         BluetoothAdapter.getDefaultAdapter();
-        FragmentHelper.switchFragment(getSupportFragmentManager(), new com.wowwee.chip_android_sampleproject.fragment.ConnectFragment(), R.id.view_id_content, false);
+        FragmentHelper.switchFragment(getSupportFragmentManager(), new ConnectFragment(), R.id.view_id_content, false);
     }
 
     @Override
@@ -43,7 +48,7 @@ public class toDogActivity extends AppCompatActivity {
             robot.disconnect();
         }
 
-        BluetoothRobot.unbindBluetoothLeService(toDogActivity.this);
+        BluetoothRobot.unbindBluetoothLeService(MainActivity.this);
 
         System.exit(0);
     }
